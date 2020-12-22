fdd97f6b7c7b9b2ccad7967306a4aeebf49c7055
elinordin <elin.evelina@live.se>
2020-12-22 17:58:54 +0100

    creating and styling navbar, adding images to src folder

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import Logo from '../images/logos/internago.png'
+
+
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
@@ -22,7 +25,23 @@ const Layout = ({ location, title, children }) => {
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
+
+      
+      <nav>
+        <img src={Logo} className='internago-img'></img>
+      
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">What we offer</a></li>
+            <li><a href="#">Blog and News</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#" className='contact-btn'>Contact us</a></li>
+            <li><a href="#" className='cta-btn'>Book a demo</a></li>
+          </ul>
+      </nav>
+
       <header className="global-header">{header}</header>
+
       <main>{children}</main>
       <footer>
         © {new Date().getFullYear()}, Built with
